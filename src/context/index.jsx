8b3162bca806d8d272be7419896d39d6709a504e
Iván <ivanlitt8@gmail.com>
2023-04-15 18:31:30 -0300

    integracion con wallet metamask

---
+++ src/context/index.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, createContext } from "react";
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+    const { contract } = useContract('0x1b64adEe2DbDCBBF49065EF9eDF0eCC44ae58Ff6');
+    const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+
+    const address = useAddress();
+    const connect = useMetamask();
+
+    const publishCampaign = async (form) => {
+        try {
+            const data = await createCampaign([
+                address, // owner
+                form.title, // tittle
+                form.description,// description
+                form.target,// target
+                new Date(form.deadline).getTime(), // deadline
+                form.image // image
+            ]);
+            console.log("Contract call success", data)
+        } catch (err) {
+            console.log("Contract call failure", err)
+        }
+    }
+
+    return (
+        <StateContext.Provider value={{
+            address,
+            contract,
+            connect,
+            createCampaign: publishCampaign
+        }}>
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
