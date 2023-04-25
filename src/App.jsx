0539aef1df4358e5929e08d653d95e5011a9579d
Iván <ivanlitt8@gmail.com>
2023-04-25 17:35:41 -0300

    correccion responsive

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => (
     <div className="sm:flex hidden mr-10 relative">
       <Sidebar />
     </div>
-    <div className="flex-1 max-sm:w-flull max-w-[1280px] mx-auto sm:pr-5">
+    <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
